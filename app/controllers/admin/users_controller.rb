b7079e417651774bbb239cf3130ed973325c446c
Frederic de Villamil <neuro@macbook-pro-de-frederic-de-villamil.local>
2008-06-15 21:02:28 +0200

    Moving lists to index, fixing some bugs, adding some translation and i18n

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,11 @@
 class Admin::UsersController < Admin::BaseController
 
-  def index
-    list
-    render :action => 'list'
+  def list
+    index
+    render :action => 'index'
   end
 
-  def list
+  def index
     @users = User.find :all
   end
 
@@ -13,7 +13,7 @@ class Admin::UsersController < Admin::BaseController
     @user = User.new(params[:user])
     setup_profiles
     if request.post? and @user.save
-      flash[:notice] = 'User was successfully created.'
+      flash[:notice] = _('User was successfully created.')
       redirect_to :action => 'list'
     end
   end
@@ -23,7 +23,7 @@ class Admin::UsersController < Admin::BaseController
     setup_profiles
     @user.attributes = params[:user]
     if request.post? and @user.save
-      flash[:notice] = 'User was successfully updated.'
+      flash[:notice] = _('User was successfully updated.')
       redirect_to :action => 'list'
     end
   end
