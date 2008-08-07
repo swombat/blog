725f57fc1176824eaa3d11c6947f9c6defac378c
Daniel Tenner <daniel.git@tenner.org>
2008-08-07 20:39:54 +0100

    Deleted bad migration

--- db/migrate/069_add_hit_count_to_articles.rb
+++
@@ -1,9 +0,0 @@
-class AddHitCountToArticles < ActiveRecord::Migration
-  def self.up
-    add_column :contents, :hit_count, :integer
-  end
-
-  def self.down
-    remove_column :contents, :hit_count
-  end
-end
